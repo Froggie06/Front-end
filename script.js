6275afc24bdd8c755029d7a44f0b4d4753e5d6a9
Myrthe Wolters <90632758+Froggie06@users.noreply.github.com>
2025-11-24 22:53:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+console.log("hi");
+
+// link naar hamburgermenu scherm
+const hamMenu = document.querySelector(".ham-menu");
+
+// link naar hamburgermenu icoon
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+// actie van het hamburger menu door te klikken
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
