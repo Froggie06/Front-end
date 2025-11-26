dbd6e659967d1aa3bb6ba64885528af9aecd4117
Myrthe Wolters <90632758+Froggie06@users.noreply.github.com>
2025-11-26 21:28:39 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,27 @@
 // JavaScript Document
 console.log("hi");
+
+// link naar hamburgermenu scherm
+const hamMenu = document.querySelector(".ham-menu");
+
+// link naar hamburgermenu icoon
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+// carrousel 
+const list = document.querySelector('.carrousel ul');
+const itemWidth = list.querySelector('li').offsetWidth + 20; // item + gap
+
+// actie van het hamburger menu door te klikken
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
+
+
+document.getElementById('next-btn').addEventListener('click', () => {
+    list.scrollLeft += itemWidth * 2;
+});
+
+document.getElementById('prev-btn').addEventListener('click', () => {
+    list.scrollLeft -= itemWidth * 2;
+});
